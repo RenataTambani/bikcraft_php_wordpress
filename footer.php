5c0120f1594fa7b29df7ab7cd139aafe9c816ca0
RenataTambani <renatatambani@gmail.com>
2022-03-23 14:28:25 -0300

    page-portoflio

--- footer.php
+++ footer.php
@@ -48,7 +48,7 @@
 	<script>window.jQuery || document.write('<script src="js/libs/jquery-1.11.2.min.js"><\/script>')</script>
 	<script src="js/plugins.js"></script>
 	<script src="js/main.js"></script>
-	<!-- JavaScript -->
+	<!-- JavaScript 
 
 	<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -58,7 +58,7 @@
 
 	  ga('create', 'UA-60088262-1', 'auto');
 	  ga('send', 'pageview');
-
+-->
 	</script>
 
     <!--inicio Wordpress Footer-->
